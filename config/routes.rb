fdc7c21d92a8decf252a7717ed3ebca4ccec55d7
Romain BERNARD <romain.bernard@edhec.com>
2015-06-16 16:05:30 +0200

    added list + edit + creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :tasks
+  get    'tasks',          to: 'tasks#index'
+  get    'tasks/:id',      to: 'tasks#show'
+  get    'tasks/new',      to: 'tasks#new'
+  post   'tasks',          to: 'tasks#create'
+  get    'tasks/:id/edit', to: 'tasks#edit'
+  patch  'tasks/:id',      to: 'tasks#update'
+  delete 'tasks/:id',      to: 'tasks#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
